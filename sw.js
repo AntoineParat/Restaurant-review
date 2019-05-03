b8ba90169073cdc4e90242b15e84cbcb881e5d84
Antoine Parat <48555841+AntoineParat@users.noreply.github.com>
2019-05-03 10:59:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+var staticCacheName = "v1";
+var urlsToCache = [
+  "./",
+  "index.html",
+  "restaurant.html",
+  "css/styles.css",
+  "data/restaurants.json",
+  "js/dbhelper.js",
+  "js/main.js",
+  "js/restaurant_info.js",
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/9.jpg",
+  "img/10.jpg"
+];
+
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ["v1"];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
